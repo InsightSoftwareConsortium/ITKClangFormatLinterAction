b4ccad787fb5dc28a31298c5da07dd81d96322b3
Matt McCormick <matt@mmmccormick.com>
2024-12-20 15:50:52 -0500

    ENH: Add an argument to specify the ITK branch to fetch .clang-format
    ITK 6 has a newer .clang-format style file for the latest clang-format.
    
    We also need the same branches clang-format.bash so the correct version
    of clang-format is checked.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,22 @@
 set -euo pipefail
 
 error_message="$1"
+itk_branch="$2"
 
 git config --global --add safe.directory /github/workspace
 
 cd "$GITHUB_WORKSPACE"
 
+# Fetch the .clang-format file from the specified branch
+wget https://raw.githubusercontent.com/InsightSoftwareConsortium/ITK/${itk_branch}/.clang-format -O /ITK.clang-format
+
+wget https://raw.githubusercontent.com/InsightSoftwareConsortium/ITK/${itk_branch}/Utilities/Maintenance/clang-format.bash
+chmod +x ./clang-format.bash
+
 if ! test -f ./.clang-format; then
   cp /ITK.clang-format ./.clang-format
 fi
-/clang-format.bash --tracked
+./clang-format.bash --tracked
 if ! git diff-index --diff-filter=M --quiet HEAD -- ':!.clang-format'; then
   echo "::error ::${error_message}"
   echo ""
